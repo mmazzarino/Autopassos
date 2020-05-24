69c20230819f8368f9bc0146a833237b73e4d26d
mmazzarino <matheus.mazzarino@acad.pucrs.br>
2020-05-23 22:03:24 -0300

    esqueleto

---
+++ src/main/java/br/com/oficina/autopassos/AutopassosApplication.java
@@ -0,0 +1,13 @@
+package br.com.oficina.autopassos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutopassosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutopassosApplication.class, args);
+	}
+
+}
