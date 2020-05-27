77d3f03d4a0a9768ca3fed139c9f5bded4d97319
mmazzarino <matheus.mazzarino@acad.pucrs.br>
2020-05-27 01:34:58 -0300

    formulario cliente

--- src/main/java/br/com/oficina/autopassos/controller/PessoasController.java
+++ src/main/java/br/com/oficina/autopassos/controller/PessoasController.java
@@ -1,34 +1,38 @@
 package br.com.oficina.autopassos.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 import br.com.oficina.autopassos.entity.Pessoas;
 import br.com.oficina.autopassos.repository.PessoasRepository;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/cadastroCliente")
 public class PessoasController {
-
-	private PessoasRepository pessoasRepository;
-
-	@Autowired
-	public PessoasController(PessoasRepository pessoaRepository) {
-		this.pessoasRepository = pessoaRepository;
+	
+	  private PessoasRepository pessoasRepository;
+	  
+	  @Autowired public PessoasController(PessoasRepository pessoaRepository) {
+	  this.pessoasRepository = pessoaRepository; }
+	 
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String formularioCliente() {
+		return "cadastroCliente";
 	}
 
-	@RequestMapping(value = "/{pessoa}", method = RequestMethod.GET)
-	public String listaPessoas(@PathVariable("pessoa") String pessoa, Model model) {
-		List<Pessoas> listaPessoas = pessoasRepository.findByNomePessoa(pessoa);
-		
-		  if (listaPessoas != null) { model.addAttribute("pessoas", listaPessoas); 
-		  }
-		 
-		return "/login";
-	}
+	
+	  @RequestMapping(method = RequestMethod.POST) 
+	  public String adicionaPessoa(Pessoas pessoa) { 
+		  pessoa.setTipoPessoa('C');
+		  pessoasRepository.save(pessoa);
+	  
+	 return "redirect:/"; }
+	 
+	
+	
 }
+
+
--- src/main/java/br/com/oficina/autopassos/entity/UF.java
+++ src/main/java/br/com/oficina/autopassos/entity/UF.java
@@ -2,6 +2,8 @@ package br.com.oficina.autopassos.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,6 +12,7 @@ import javax.persistence.Table;
 public class UF {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO) 
 	@Column(name="CodUF")
     private Integer codUf; 
 	
